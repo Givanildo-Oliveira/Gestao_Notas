0290b51136ab5130d23f914a9ed9b2a7979f672b
Givanildo Oliveira <skorpion_pi1@hotmail.com>
2026-09-10 23:49:19 -0300

    Configuração do Sistema de Notas

---
+++ src/main/java/br/com/gos/devshowcase/Gestao_Notas2/GestaoNotas2Application.java
@@ -0,0 +1,13 @@
+package br.com.gos.devshowcase.Gestao_Notas2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoNotas2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoNotas2Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gos/devshowcase/Gestao_Notas2/GestaoNotas2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gos.devshowcase.Gestao_Notas2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoNotas2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
